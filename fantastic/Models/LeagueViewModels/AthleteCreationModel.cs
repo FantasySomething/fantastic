9b89de090c062b1c16692ddc7bd9d8fa19d62838
MarMegh <martinmm@outlook.com>
2017-12-14 14:23:52 -0800

    Database Updated to account for teams. Also, my DB includes prepopulated Sports teams: 1: Baseball 2: Basketball 3: Football 4: Tennis 5: Soccer

---
+++ fantastic/Models/LeagueViewModels/AthleteCreationModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace fantastic.Models.LeagueViewModels
+{
+    public class AthleteCreationModel
+    {
+        [Required]
+        [Display(Name = "Sport")]
+        public int sport { get; set; }
+
+        [Required]
+        [Display(Name = "Season")]
+        public int season { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public int Name { get; set; }
+    }
+}
