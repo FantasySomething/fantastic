ca5f0f1cbaefeeaf34d878d7d24ada31e64ec12c
Lex Myers <lex.myers@gmail.com>
2017-12-15 12:33:58 -0800

    Initialised test project

---
+++ SportTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SportTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
